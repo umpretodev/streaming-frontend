22864a30868988c148e16aebd7a1923b461c2649
Pedro Fernandes <lebackman18@gmail.com>
2025-05-06 23:40:53 -0300

    ✨ feat: react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Users from './pages/Users/Users';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/users" element={<Users></Users>}></Route>
         <Route path="*" element={<Navigate to="/" replace></Navigate>}></Route>
       </Routes>
     </BrowserRouter>
